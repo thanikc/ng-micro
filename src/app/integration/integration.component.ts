30eb3a4ff39d1cccdff0e4add402125fc1e660d6
Thanik Cheowtirakul <thanikc@gmail.com>
2019-02-08 13:06:43 +0100

    structure item can contain additional dependencies and properties

--- src/app/integration/integration.component.ts
+++ src/app/integration/integration.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit, OnDestroy, ViewChild, ElementRef, SimpleChanges } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Subscription } from 'rxjs';
 import { select, Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+import { tap, map, switchMapTo, distinctUntilChanged } from 'rxjs/operators';
 
 import { AppState } from 'app/store/state/app.state';
 import { selectSelectedItem, selectItems } from 'app/store/selectors/structure.selectors';
 import { GetStructureItem } from 'app/store/actions/structure.action';
-import { tap, map, switchMapTo, distinctUntilChanged } from 'rxjs/operators';
+import { StructureItem } from 'app/models/structure.interface';
 
 @Component({
   selector: 'app-integration',
@@ -18,14 +19,19 @@ export class IntegrationComponent implements OnInit, OnDestroy {
 
   structureItem$ = this.store.pipe(select(selectSelectedItem));
   private sub: Subscription;
-  
+
   constructor(private store: Store<AppState>, private route: ActivatedRoute) { }
-  
+
   ngOnInit() {
     this.sub = this.store.pipe(select(selectItems)).pipe(
       distinctUntilChanged(),
       switchMapTo(this.structureItem$),
-      tap(item => { if (item) { this.setupComponent(item.id, item.src); } }),
+      tap(item => {
+        if (item) {
+          this.setupDependencies(item);
+          this.setupComponent(item);
+        }
+      }),
       switchMapTo(this.route.params),
       map(params => this.store.dispatch(new GetStructureItem(params.id))),
     ).subscribe();
@@ -35,19 +41,33 @@ export class IntegrationComponent implements OnInit, OnDestroy {
     this.sub.unsubscribe();
   }
 
-  setupComponent(id: string, src: string) {
+  setupComponent(item: StructureItem): void {
     const content = this.content.nativeElement;
     while (content.firstChild) {
       content.removeChild(content.firstChild);
     }
 
-    const elem = document.createElement(id);
-    content.appendChild(elem);
+    const elem = document.createElement(item.id);
+    for (let key in item.props) {
+      elem.setAttribute(key, item.props[key]);
+    }
+    
+    content.appendChild(elem);    
+  }
 
-    const script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = src;
+  setupDependencies(item: StructureItem): void {
+    for (let src of item.dependencies || []) {
+      this.createScript(src);
+    }
+    this.createScript(item.src);
+  }
+
+  createScript(src: string, async = false): void {
     if (!document.querySelector(`script[src="${src}"]`)) {
+      const script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.src = src;
+      script.async = async;
       document.body.appendChild(script);
     }
   }
--- src/app/models/structure.interface.ts
+++ src/app/models/structure.interface.ts
@@ -2,4 +2,6 @@ export interface StructureItem {
   id: string;
   title: string;
   src: string;
+  dependencies?: string[];
+  props?: { [key: string]: any }
 }
