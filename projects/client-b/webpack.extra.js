65a3426539a65bd1aadc50406f76a714ef4f5246
Thanik Cheowtirakul <thanikc@gmail.com>
2019-03-06 14:57:30 +0100

    use angular and rxjs as shared external dependencies

--- projects/client-b/webpack.extra.js
+++ projects/client-b/webpack.extra.js
@@ -1,3 +1,10 @@
+const { Externals } = require('share-loader');
+
 module.exports = {
-  
-}
+    externals: [
+        Externals({
+            namespace: 'portal',
+            modules: [[/@angular/, /rxjs/]]
+        })
+    ],
+};
--- webpack.extra.js
+++ webpack.extra.js
@@ -1,3 +1,17 @@
 module.exports = {
-  
-}
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                use: [{
+                    loader: 'share-loader',
+                    options: {
+                        modules: [/@angular/, /rxjs/],
+                        namespace: 'portal'
+                    }
+                }]
+            }
+        ]
+
+    }
+};
